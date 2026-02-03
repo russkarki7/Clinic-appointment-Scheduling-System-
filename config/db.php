fd037a8b133a88ed618e86e65be2b4ad1e698a40
russkarki7 <russkarki7@gmail.com>
2026-02-03 09:27:20 +0545

    Add files via upload

--- config/db.php
+++ config/db.php
@@ -5,11 +5,13 @@ $user = "np03cs4s240191";
 $password = "VCbZA5VhUN";          
 $database = "np03cs4s240191"; 
 
+
+
 $conn = new mysqli($host, $user, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
-    die("❌ Database connection failed: " . $conn->connect_error);
+    die("Database connection failed: " . $conn->connect_error);
 }
 
 $conn->set_charset("utf8");
